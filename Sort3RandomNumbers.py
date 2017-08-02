98caab95e8e07b3c433dd830a6075a6c0228f4f8
mahvashj <mahvash.nejad@gmail.com>
2017-08-02 10:55:38 -0700

    Create Sort3RandomNumbers.py

---
+++ Sort3RandomNumbers.py
@@ -0,0 +1,15 @@
+firstNum = input('Enter a number:\n')
+secondNum = input('Enter a number:\n')
+thirdNum = input('Enter a number:\n')
+numList = [firstNum, secondNum, thirdNum]
+numList.sort()
+print(numList[0],',', numList[1],',', numList[2])
+
+# The printed result is listed below. Not part of the code
+# Enter a number:
+# 5
+# Enter a number:
+# 2
+# Enter a number:
+# 9
+# 2 , 5 , 9
